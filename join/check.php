582f5c72fc166348f007a7374c85967f7ec08518
shinyahirai <908.shinya@gmail.com>
2016-03-17 08:16:46 +0800

    Initial commit

---
+++ join/check.php
@@ -0,0 +1,47 @@
+<?php
+?>
+
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>Seed SNS</title>
+</head>
+<body>
+  <h1>会員登録</h1>
+  <p>記入した入力内容を確認して、「登録する」ボタンをクリックしてください</p>
+
+  <form action="" method="" enctype="multipart/form-data">
+
+    <dl>
+      <dt>ニックネーム</dt>
+      <dd>
+      </dd>
+      <dt>メールアドレス</dt>
+      <dd>
+      </dd>
+      <dt>パスワード</dt>
+      <dd>
+        【表示されません】
+      </dd>
+      <dt>写真</dt>
+      <dd>
+      </dd>
+    </dl>
+    <div>
+      <a href="index.php?action=rewrite">&laquo;&nbsp;書き直す</a>
+      <input type="submit" value="登録する">
+    </div>
+
+  </form>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
---
+++ join/index.php
@@ -0,0 +1,39 @@
+<?php
+?>
+
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>Seed SNS</title>
+</head>
+<body>
+  <h1>会員登録</h1>
+  <p>次のフォームに必要事項をご記入ください。</p>
+
+  <form action="" method="" enctype="multipart/form-data">
+
+    <dl>
+      <dt>ニックネーム <span class="required">必須</span></dt>
+      <dd><input type="text" name="nick_name"></dd>
+      <dt>メールアドレス <span class="required">必須</span></dt>
+      <dd><input type="text" name="email"></dd>
+      <dt>パスワード <span class="required">必須</span></dt>
+      <dd><input type="password" name="password"></dd>
+      <dt>写真 <span class="required">必須</span></dt>
+      <dd><input type="file" name="image"></dd>
+    </dl>
+    <div><input type="submit" value="入力内容を確認する"></div>
+
+  </form>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
